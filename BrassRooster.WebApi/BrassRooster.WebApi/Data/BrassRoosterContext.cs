588cad65400e4d93fdc22c1cbe7ab27b75e34b6f
Kristi Sik <jemynanyt@gmail.com>
2022-04-09 17:16:38 +0300

    BR-6 Init EF DB context. Fix project name.

---
+++ BrassRooster.WebApi/BrassRooster.WebApi/Data/BrassRoosterContext.cs
@@ -0,0 +1,19 @@
+﻿using BrassRooster.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+namespace BrassRooster.WebApi.Data
+{
+    public class BrassRoosterContext : DbContext
+    {
+        public BrassRoosterContext(DbContextOptions<BrassRoosterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<TestServer> TestServers { get; set; }
+    }
+}
--- BrassRooster.WebApi/BrassRooster.WebApi/Utility/UsageState.cs
+++ BrassRooster.WebApi/BrassRooster.WebApi/Utility/UsageState.cs
@@ -1,4 +1,4 @@
-﻿namespace BrassRoaster.WebApi.Utility
+﻿namespace BrassRooster.WebApi.Utility
 {
     public enum UsageState
     {
